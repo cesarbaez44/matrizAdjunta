a6904403afd3ed8733da7e80ede8b9ed4009d2ab
Cesar <cesarbaezmoya06@gmail.com>
2025-11-22 23:25:52 -0600

    escritura y calcular la matriz adj

--- matrizFilesAdj.java
+++ matrizFilesAdj.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 public class matrizFilesAdj {
 
@@ -33,11 +35,79 @@ public class matrizFilesAdj {
                 i++;
             }
             reader.close();
-            
+
         } catch(Exception e){
             System.out.println("Error al leer el archivo: " + e.toString());
         }
         return matriz;
+    }
+    // Escribir matriz en archivo
+    public void writeMatrixToFile(String fileName, double[][] matriz){
+        try{
+            FileWriter file = new FileWriter("C:\\archivos\\" + fileName);
+            PrintWriter writer = new PrintWriter(file);
+            for(int i=0; i<matriz.length; i++){
+                for(int j=0; j<matriz[i].length; j++){
+                    writer.print(matriz[i][j] + " ");
+                }
+                writer.println();
+            }
+            file.close();
+        } catch(Exception e){
+            System.out.println("Error al escribir el archivo: " + e.toString());
+        }
+    }
+
+    // Calcula la matriz adjunta 
+    public double[][] adjoint(double[][] A){
+        int n = A.length;
+        double[][] adj = new double[n][n];
+
+        if(n == 1){
+            adj[0][0] = 1;
+            return adj;
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                double[][] temp = getMinor(A, i, j);
+                double det = determinant(temp);
+                adj[j][i] = ((i+j)%2==0 ? 1 : -1) * det;
+            }
+        }
+        return adj;
+    }
+
+    // Obtiene la submatriz eliminando fila p y columna q
+    private double[][] getMinor(double[][] A, int p, int q){
+        int n = A.length;
+        double[][] minor = new double[n-1][n-1];
+        int i2=0;
+        for(int i=0; i<n; i++){
+            if(i==p) continue;
+            int j2=0;
+            for(int j=0; j<n; j++){
+                if(j==q) continue;
+                minor[i2][j2] = A[i][j];
+                j2++;
+            }
+            i2++;
+        }
+        return minor;
+    }
+
+    // Determinante recursivo
+    private double determinant(double[][] A){
+        int n = A.length;
+        if(n==1) return A[0][0];
+        if(n==2) return A[0][0]*A[1][1] - A[0][1]*A[1][0];
+
+        double det=0;
+        for(int j=0; j<n; j++){
+            double[][] minor = getMinor(A,0,j);
+            det += ( (j%2==0?1:-1) * A[0][j] * determinant(minor) );
+        }
+        return det;
     }
 }
     
